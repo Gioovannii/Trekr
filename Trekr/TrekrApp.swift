009913beeaac0b50a8a8c63917ccbd483f54c8ea
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-24 13:48:26 +0800

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Giovanni Gaffé on 2021/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
